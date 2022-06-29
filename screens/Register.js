da3d5ff8308599a911c63c07760a7144fa61b475
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-29 13:06:10 +0300

    Styled the login screen

--- screens/Register.js
+++ screens/Register.js
@@ -1,77 +1,183 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   TextInput,
-  CheckBox,
   TouchableOpacity,
   ScrollView,
+  StatusBar,
+  SafeAreaView,
+  Dimensions,
 } from "react-native";
+import Checkbox from "expo-checkbox";
+
 import Icon from "react-native-vector-icons/Feather";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Register = () => {
+  const [isChecked, setChecked] = useState(true);
+
+  const checked = () => {
+    setChecked(false);
+  };
   return (
-    <ScrollView>
-      <View>
-        <Text>Login</Text>
-        <Text>By Signing in you are agreeing</Text>
-        <View style={{ display: "flex", flexDirection: "row" }}>
-          <Text>Our</Text>
-          <Text style={{ color: "blue" }}>Terms and privacy policy</Text>
-        </View>
-        <View style={{ display: "flex", flexDirection: "row", marginTop: 10 }}>
-          <Text style={{ textDecorationLine: "underline" }}>Login </Text>
-          <Text>Register </Text>
-        </View>
-        <View>
-          <View style={{ flex: 1 }}>
-            <Icon name="mail" size={22} />
-            <TextInput placeholder="Email Address" />
+    <SafeAreaView
+      style={{ marginTop: StatusBar.currentHeight, backgroundColor: "#7696db" }}
+    >
+      <View
+        style={{
+          backgroundColor: "white",
+          borderBottomLeftRadius: 100,
+          borderBottomRightRadius: 100,
+          height: Dimensions.get("window").height * 0.8,
+        }}
+      >
+        <View
+          style={{
+            padding: 40,
+          }}
+        >
+          <View style={{ alignItems: "center" }}>
+            <Text style={{ fontSize: 25, fontWeight: "bold" }}>Login</Text>
+            <Text style={{ fontSize: 18, marginTop: 10 }}>
+              By Signing in you are agreeing
+            </Text>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                fontSize: 18,
+                marginTop: 5,
+              }}
+            >
+              <Text>Our</Text>
+              <Text style={{ color: "blue" }}>Terms and privacy policy</Text>
+            </View>
           </View>
 
-          <View style={{ flex: 1 }}>
-            <Icon name="lock" size={22} />
-            <TextInput placeholder="Password" />
-            <Icon name="eye" size={22} />
-          </View>
           <View
             style={{
               display: "flex",
               flexDirection: "row",
-              justifyContent: "space-between",
+              marginTop: 25,
+              justifyContent: "center",
             }}
           >
-            <View style={{ flexDirection: "row" }}>
-              <CheckBox />
-              <Text>Remember Password</Text>
+            <Text
+              style={{
+                textDecorationLine: "underline",
+                marginRight: 10,
+                fontWeight: "bold",
+                fontSize: 18,
+                color: "blue",
+              }}
+            >
+              Login
+            </Text>
+            <Text style={{ fontSize: 18 }}>Register </Text>
+          </View>
+          <View style={{ marginTop: 30 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                borderBottomWidth: 1,
+                borderBottomColor: "#808080",
+                marginBottom: 30,
+                alignItems: "center",
+              }}
+            >
+              <Icon name="mail" size={18} color="#808080" />
+              <TextInput
+                style={{ marginLeft: 10 }}
+                placeholder="Email Address"
+              />
             </View>
 
-            <Text>Forgot Password</Text>
-          </View>
+            <View
+              style={{
+                flexDirection: "row",
 
-          <TouchableOpacity
-            style={{ backgroundColor: "blue", borderRadius: 10 }}
-          >
-            <Text style={{ color: "white", b }}>Login</Text>
-          </TouchableOpacity>
-          <Text>or connect with</Text>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-around" }}
-          >
-            <Ionicons name="logo-facebook" size={24} />
-            <Ionicons name="logo-instagram" size={24} />
-            <Ionicons name="logo-pinterest" size={24} />
-            <Ionicons name="logo-linkedin" size={24} />
-          </View>
+                borderBottomWidth: 1,
+                borderBottomColor: "#808080",
+                marginBottom: 30,
+                alignItems: "center",
+              }}
+            >
+              <Icon name="lock" size={22} color="#808080" />
+              <TextInput style={{ marginLeft: 10 }} placeholder="Password" />
+            </View>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginBottom: 30,
+                  alignItems: "center",
+                }}
+              >
+                <Checkbox value={isChecked} onValueChange={checked} />
+                <Text style={{ marginLeft: 5 }}>Remember Password</Text>
+              </View>
 
-          <View>
-            <Ionicons name="finger-print-outline" size={30} />
-            <Text>Login with touch id</Text>
+              <Text style={{ color: "blue" }}>Forgot Password</Text>
+            </View>
+
+            <TouchableOpacity
+              style={{
+                backgroundColor: "blue",
+                borderRadius: 10,
+                height: 35,
+                justifyContent: "center",
+                alignItems: "center",
+                marginBottom: 15,
+              }}
+            >
+              <Text style={{ color: "white" }}>Login</Text>
+            </TouchableOpacity>
+            <Text style={{ alignSelf: "center" }}>or connect with</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                marginTop: 10,
+              }}
+            >
+              <Ionicons name="logo-facebook" size={24} color="blue" />
+              <Ionicons name="logo-instagram" size={24} color="blue" />
+              <Ionicons name="logo-pinterest" size={24} color="red" />
+              <Ionicons name="logo-linkedin" size={24} color="#5679c4" />
+            </View>
           </View>
         </View>
       </View>
-    </ScrollView>
+
+      <View
+        style={{
+          alignItems: "center",
+          height: Dimensions.get("window").height * 0.2,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            height: 80,
+            width: 80,
+            borderRadius: 10,
+            backgroundColor: "#5c87e6",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Ionicons name="finger-print-outline" size={40} color="white" />
+        </TouchableOpacity>
+
+        <Text style={{ color: "white" }}>Login with touch id</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
